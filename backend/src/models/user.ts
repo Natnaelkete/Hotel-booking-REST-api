fda3e170480a8183a14fdaa4b60ab931637fd9b9
Natnaelkete <ketemanatnael023@gmail.com>
2025-07-03 03:53:22 -0700

    add jwt and cookie for registration

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -11,7 +11,6 @@ export type UserType = {
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import User from "../models/user";
 
 const router = express.Router();
 
-router.get("/register", async (req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
   try {
     let user = await User.findOne({
       email: req.body.email,
@@ -15,7 +16,22 @@ router.get("/register", async (req: Request, res: Response) => {
 
     user = new User(req.body);
 
-    user.save();
+    await user.save();
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY as string,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+    return res.sendStatus(200);
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "Something went wrong" });
