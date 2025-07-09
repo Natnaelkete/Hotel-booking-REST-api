432775d318d762b99281fc8819087633b5a0dd16
Natnaelkete <ketemanatnael023@gmail.com>
2025-07-09 08:01:07 -0700

    setup e2e testing

--- backend/src/config/DB.ts
+++ backend/src/config/DB.ts
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL as string);
-    console.log(`MongoDb Connected:${conn.connection.host}`);
+    const conn = await mongoose
+      .connect(process.env.MONGO_URL as string)
+      .then((con) => console.log(`MongoDb Connected:${con.connection.host}`));
   } catch (error) {
     console.log(`Error: ${error}`);
     process.exit(1);
